13034d8f7285936a2f608ce0ff914de65c1dd686
Borhan <brborhan70@gmail.com>
2025-08-16 00:43:09 +0600

    Explore special types

--- src/script.ts
+++ src/script.ts
@@ -24,3 +24,44 @@ type UserId = string | number;
 function getUser(id: UserId) {
   console.log(`Fetching user with ID: ${id}`);
 }
+
+//এবার আমরা TypeScript-এর Special Types দেখবো।
+
+// 1️⃣ any Type
+// any lets a variable hold any type, like turning off TypeScript’s type checking.
+
+let data: any;
+data = 123; // ✅ OK
+data = "Hello"; // ✅ OK
+data = true; // ✅ OK
+
+// 2️⃣ unknown Type
+// unknown is safer than any. You can assign any type, but you must check the type before using it.
+
+let value: unknown;
+value = 123;
+value = "Hello";
+
+// Type narrowing is required
+if (typeof value === "string") {
+  console.log(value.toUpperCase());
+}
+
+// 3️⃣ void Type
+// English: Represents no value — usually for functions that don’t return anything.
+
+function logMessage(msg: string): void {
+  console.log(msg);
+}
+
+// 4️⃣ never Type
+//  Represents a value that never occurs — functions that never return or always throw errors.
+
+function throwError(message: string): never {
+  throw new Error(message);
+}
+
+// 5️⃣ null & undefined
+//  Represent absence of a value. You can use them directly or in union types.
+let user: string | null = null;
+let ages: number | undefined = undefined;
